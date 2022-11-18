16fab86dfa7ed8c228be4453227b49d43e7cfe2a
Niranjan Dahal <85100536+niranjandahal@users.noreply.github.com>
2022-11-18 18:11:27 +0545

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:o1loginsignupphp/ui/login.dart';
+import 'package:o1loginsignupphp/ui/signup.dart';
 
-Widget description(context, name,age) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text(name.toString()),
-    ),
-    body: Center(
-      child: Text(age.toString()),
-    ),
-  );
+void main() {
+  runApp(const MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        hintColor: Colors.grey.withOpacity(0.7),
+      ),
+      debugShowCheckedModeBanner: false,
+         home: const MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue.shade300,
+      body: loginpageui(),
+    );
+  }
+}
+
+
+
